5ef71a979b831ed18e89538ec87ecaf13415a5c8
Aidens-fox <rootwang838@gmail.com>
2026-03-14 17:16:53 +0800

    Add files via upload

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,10 @@
 ;; 主题配置
 (load-theme 'gruvbox-dark-medium t)
+;;自动换行
+(global-visual-line-mode 1) ; 1表示开启，0表示关闭
+;;欢迎界面
+(setq inhibit-startup-message t)
+(setq initial-buffer-choice "~/")
 ;; 状态栏显示其他信息
 (display-battery-mode 1)
 (display-time-mode 1)
@@ -8,10 +13,15 @@
 (menu-bar-mode -1)
 ;; 隐藏工具栏
 (tool-bar-mode -1)
+;;字体设置
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font-20")
+(add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font-20"))
 ;; 隐藏滚动条
 (when (display-graphic-p) (toggle-scroll-bar -1))
 ;; 自动补全括号
 (electric-pair-mode t)
+;;剪贴板共享
+(setq x-select-enable-clipboard t)
 ;; 显示行号
 (global-display-line-numbers-mode 1)
 ;; 插件管理器
